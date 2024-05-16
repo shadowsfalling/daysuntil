42e6a3f5d1a540291725c3b8860209d5127c6171
shadowsfalling <sascha@saschasels.de>
2024-05-16 09:39:23 +0200

    java: added init project

---
+++ server/java/src/main/java/exampleProject/daysUntil/DaysUntilApplication.java
@@ -0,0 +1,13 @@
+package exampleProject.daysUntil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaysUntilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaysUntilApplication.class, args);
+	}
+
+}

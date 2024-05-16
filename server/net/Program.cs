36c58d2ff1d32fe4bc4e1b588d09252fbe92cd53
shadowsfalling <sascha@saschasels.de>
2024-05-16 16:17:06 +0200

    dotnet: added swagger docu

--- server/net/Program.cs
+++ server/net/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using System.Security.Cryptography;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 Console.WriteLine("Your Secret key: ");
 Console.WriteLine(JwTTokenGenerator.GenerateSecureSecretKey());
@@ -16,7 +17,12 @@ builder.Services.AddDbContext<DaysUntilContext>(options =>
 });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddSwaggerGen(c =>
+   {
+       c.SwaggerDoc("v1", new OpenApiInfo { Title = "DaysUntil API", Version = "v1" });
+   });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -68,7 +74,11 @@ app.UseAuthorization();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = "swagger"; // Set the Swagger UI at the app's root
+});
 }
 
 app.UseHttpsRedirection();

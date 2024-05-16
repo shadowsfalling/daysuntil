733f5c8ff30d371084da3433f2dec03e7298d6e1
shadowsfalling <sascha@saschasels.de>
2024-05-16 17:27:30 +0200

    ionic: [feature] added redirect to login, show countdown

--- frontend/ionic/src/app/interceptors/auth.interceptor.ts
+++ frontend/ionic/src/app/interceptors/auth.interceptor.ts
@@ -1,11 +1,14 @@
 
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  
+
+  constructor(private router: Router) { }
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authToken = this.getToken();
 
@@ -13,7 +16,18 @@ export class AuthInterceptor implements HttpInterceptor {
       headers: request.headers.set('Authorization', `Bearer ${authToken}`)
     });
 
-    return next.handle(authReq);
+    return next.handle(authReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+
+        console.log("error", error);
+
+        if (error.status === 403 && error.error === "invalid token") {
+          // Benutzer auf die Login-Seite umleiten
+          this.router.navigate(['/login']);
+        }
+        return throwError(error);
+      })
+    );
   }
 
   private getToken(): string {
--- frontend/ionic/src/app/pages/countdown/show-countdown/show-countdown.page.ts
+++ frontend/ionic/src/app/pages/countdown/show-countdown/show-countdown.page.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountdownService } from 'src/app/services/countdown.service';
 
 @Component({
   selector: 'app-show-countdown',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowCountdownPage implements OnInit {
 
-  constructor() { }
+  id: number = null!;
+  title: string = '';
+  datetime: Date = null!;
+  countdownText: string = '';
+  isOwner = true; // todo: check if user is really the owner
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private http: HttpClient, private countdownService: CountdownService) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.getCountdownDetails();
+  }
+
+  getCountdownDetails() {
+
+    this.countdownService.getCountdownById(this.id)
+    .subscribe({
+      next: (data) => {
+        this.title = data.title;
+        this.datetime = new Date(data.datetime);
+        this.updateCountdown();
+        setInterval(() => this.updateCountdown(), 1000);
+      },
+      error: (error) => {
+        console.error('Error loading countdown data', error);
+      }
+    });
+  }
+
+  updateCountdown() {
+    const now = new Date().getTime();
+    const countToDate = this.datetime.getTime();
+    const difference = countToDate - now;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    this.countdownText = `${days} Tage ${hours} Stunden ${minutes} Minuten ${seconds} Sekunden`;
   }
 
 }
